a22145d7e172836a5fdb86ec606d112d551d24db
Konvyru <67903137+Konvyru@users.noreply.github.com>
2023-02-13 10:45:03 +0100

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,13 +1,13 @@
 /**
  * Web application
  */
-const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/c3c8fc37-076c-4466-bb43-d3375f4c05fe/guestbook/';
+const apiUrl = 'https://us-south.functions.appdomain.cloud/api/v1/web/c3c8fc37-076c-4466-bb43-d3375f4c05fe/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/read-guestbook-entries-sequence.json`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${apiUrl}/entries`,
+      url: `${apiUrl}/save-guestbook-entry-sequence`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
